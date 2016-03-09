18d716b2b9de5bff3d823a80b49e4114abb4a493
Christopher Lewis <chris.lewis@hudl.com>
2016-03-09 13:11:53 +0000

    Adding typescript index file, can't get it to build in VS at the moment

--- src/Hudl.BugBounty.WebApp/Scripts/Home/index.tsx
+++ src/Hudl.BugBounty.WebApp/Scripts/Home/index.tsx
@@ -1,3 +1,18 @@
 ﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
+
+/// <reference path="../../typings/react/react.d.ts" />
+/// <reference path="../../typings/react/react-dom.d.ts" />
+import React =  require('react');
+import ReactDOM = require('react-dom');
+
+var Test = React.createClass<{}, {}>({
+    render: function () {
+        return (<div>
+                  Hello Bounty Hunters
+                </div>);        
+    }
+});
+
+ReactDOM.render(<Test />, document.getElementById('content')); 
