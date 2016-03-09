e932352c1ac5e96d2fb4a647840dab184dff35fc
Christopher Lewis <chris.lewis@hudl.com>
2016-03-09 10:50:03 +0000

    Adding configuration

---
+++ src/Hudl.BugBounty.WebApp/Options/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Hudl.BugBounty.WebApp.Options
+{
+    public class DatabaseSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Database { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
--- src/Hudl.BugBounty.WebApp/Startup.cs
+++ src/Hudl.BugBounty.WebApp/Startup.cs
@@ -1,22 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Hudl.BugBounty.WebApp.Options;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Hudl.BugBounty.WebApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true) // The values in here override the values in config.json if they exist
+                .AddEnvironmentVariables("DatabaseSettings:") // Get additional database settings from environment variables e.g. Username and Password since we don't want these in config.
+                .Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions(); //Enable configuration options
+            services.Configure<DatabaseSettings>(Configuration); // Get the DatabaseSettings from configuration
         }
 
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
