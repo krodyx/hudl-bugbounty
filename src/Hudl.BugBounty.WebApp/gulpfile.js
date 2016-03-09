6ef9f076ab85d96e442840f3c6ce832f20026883
Logan Laughlin <loganlaughlin@gmail.com>
2016-03-09 16:19:34 -0600

    Fixed react/browserify stuff

--- src/Hudl.BugBounty.WebApp/gulpfile.js
+++ src/Hudl.BugBounty.WebApp/gulpfile.js
@@ -18,18 +18,37 @@ var gulp = require("gulp"),
   rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify");
+  uglify = require("gulp-uglify"),
+  shell  = require("gulp-shell");
 
 var webroot = "./wwwroot/";
 
 var paths = {
     js: webroot + "scripts/**/*.js",
     minJs: webroot + "scripts/**/*.min.js",
+    ts: "Scripts/**/*.ts",
+    tsx: "Scripts/**/*.tsx",
     css: webroot + "css/**/*.css",
     minCss: webroot + "css/**/*.min.css",
     concatJsDest: webroot + "scripts/site.min.js",
     concatCssDest: webroot + "css/site.min.css"
 };
+
+gulp.task("install", shell.task([
+    'npm install -g browserify bower tsd typescript',
+    'npm install',
+    'tsd install'
+]))
+
+gulp.task("build:js", shell.task([
+    'tsc -p Scripts', 
+    'browserify wwwroot/scripts/index.js -o wwwroot/scripts/bundle.js'
+]));
+
+gulp.task("build:css", function (cb) {
+});
+
+gulp.task("build", ["build:js", "build:css"]);
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
